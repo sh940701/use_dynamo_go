672e71c373d44482c20d8ecbaa9e8e41cf3d1458
96sunghyun <visioner2168@gmail.com>
2023-05-29 23:54:46 +0900

    feat: add AddBuildings() for batch test

--- dbHandler/dbHandler.go
+++ dbHandler/dbHandler.go
@@ -67,3 +67,28 @@ func (basics TableBasics) AddBuilding(building map[string]string) error {
 
 	return err
 }
+
+func (basics TableBasics) AddBuildings(buildings []map[string]string) error {
+	var err error
+	var item map[string]types.AttributeValue
+	var writeReqs []types.WriteRequest
+	for _, building := range buildings {
+		item, err = attributevalue.MarshalMap(building)
+		if err != nil {
+			log.Printf("Couldn't marshal building %v for batch writing. Here's why: %v\n", building, err)
+		} else {
+			writeReqs = append(
+				writeReqs,
+				types.WriteRequest{PutRequest: &types.PutRequest{Item: item}},
+			)
+		}
+	}
+	_, err = basics.DynamoDbClient.BatchWriteItem(context.TODO(), &dynamodb.BatchWriteItemInput{
+		RequestItems: map[string][]types.WriteRequest{basics.TableName: writeReqs},
+	})
+	if err != nil {
+		log.Printf("Couldn't add a batch of buildings to %v. Here's why: %v\n", basics.TableName, err)
+	}
+
+	return nil
+}
