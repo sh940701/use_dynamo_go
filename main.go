406bdc63531063a3062c4cf6bf65c4377f333488
96sunghyun <visioner2168@gmail.com>
2023-05-29 23:46:39 +0900

    feat: add buildings by 25 function

--- main.go
+++ main.go
@@ -57,3 +57,43 @@ func main() {
 	}
 
 }
+
+func InsertDataBatch(inputFilePath string, basic dbHandler.TableBasics) {
+	// file config
+	inputFile, err := os.Open(inputFilePath)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer inputFile.Close()
+
+	scanner := bufio.NewScanner(inputFile)
+
+	// txt 파일의 모든 문서를 25개씩 write 요청
+	// 1080개 기준 싱글스레드 경과 시간: 988.009375ms
+	// 전체 파일 기준 싱글스레드 경과 시간: 2 시간 이상
+	var buildings []map[string]string
+	for scanner.Scan() {
+		strData := dataHandler.SplitByPipeline(scanner.Text())
+		building := map[string]string{
+			"PK":      strData[0],
+			"address": strData[6],
+		}
+		buildings = append(buildings, building)
+		if len(buildings) == 25 {
+			buildingsCopy := make([]map[string]string, len(buildings))
+			copy(buildingsCopy, buildings)
+			err := basic.AddBuildings(buildingsCopy)
+			if err != nil {
+				panic(err)
+			}
+			buildings = buildings[:0]
+		}
+	}
+
+	// 남은 항목 추가
+	err = basic.AddBuildings(buildings)
+	if err != nil {
+		panic(err)
+	}
+}
